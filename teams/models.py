baa3021c963d36988a89633f22646fac2329367b
Praisesomiji <root@PRAISESOMIJI-PC>
2024-07-27 16:35:56 +0100

    Define members for members and teams

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
-# Create your models here.
+class Team(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.group.name
+
+def Unit(model.Models):
+    name = models.CharField(max_length=255)
+    teams = models.ManyToManyField(Team)
+
