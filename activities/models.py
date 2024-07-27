b1411e24c4b76cdac12ef39ed3ed1eb93505f6fa
Praisesomiji <praise@gmail.com>
2024-07-27 20:09:20 +0100

    Migrate models of core apps

--- activities/models.py
+++ activities/models.py
@@ -3,22 +3,21 @@ from teams.models import Unit, Team
 from products.models import Production
 from members.models import Profile
 
-class Trace(models.Model):
-    productions = models.ManyToMany(Production)
-    week = models.IntegerField()
-    unit = models.ForeignKey(Unit, null=True, blank=True, on_delete=models.SET_NULL)
-    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
-    producer = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    activities = models.ManyToMany(Activity)
-
 class Activity(models.Model):
     name = models.CharField(max_length=255)
     production = models.ForeignKey(Production, on_delete=models.CASCADE)
     date = models.DateField()
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     cost_in_naira = models.DecimalField(max_digits=10, default=0, decimal_places=2)
     loss_or_mortality = models.DecimalField(max_digits=10, default=0, decimal_places=2)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    activities = models.ManyToManyField(Activity)
+
+class Trace(models.Model):
+    productions = models.ManyToManyField(Production)
+    week = models.IntegerField()
+    unit = models.ForeignKey(Unit, null=True, blank=True, on_delete=models.SET_NULL)
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
+    producer = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
 
--- members/models.py
+++ members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=[('active', 'Active'), ('inactive', 'Inactive')])
 
